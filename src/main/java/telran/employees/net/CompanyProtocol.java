973bb982e054822fe3cb265b817da8da21057c36
Yuragranovsky <yuragranovsky@gmail.com>
2024-07-25 21:35:05 +0300

    company server appl initial code

---
+++ src/main/java/telran/employees/net/CompanyProtocol.java
@@ -0,0 +1,91 @@
+package telran.employees.net;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import telran.employees.Company;
+import telran.employees.Employee;
+import telran.employees.Manager;
+import telran.net.Protocol;
+import telran.net.Request;
+import telran.net.Response;
+import telran.net.ResponseCode;
+public class CompanyProtocol implements Protocol {
+	Company company;
+	
+	public CompanyProtocol(Company company) {
+		this.company = company;
+	}
+
+	@Override
+	public Response getResponse(Request request) {
+		String requestType = request.requestType();
+		String requestData = request.requestData();
+		Response response = null;
+		try {
+			response = switch(requestType) {
+			case "addEmployee" -> addEmployee(requestData);
+			case "getEmployee" -> getEmployee(requestData);
+			case "removeEmployee" -> removeEmployee(requestData);
+			case "getDepartmentBudget" -> getDepartmentBudget(requestData);
+			case "getDepartments" -> getDepartments(requestData);
+			case "getManagersWithMostFactor" -> getManagersWithMostFactor(requestData);
+			default -> wrongTypeResponse(requestType);
+			};
+			
+		} catch (Exception e) {
+			response = wrongDataResponse(e.getMessage());
+		}
+		return response;
+	}
+
+	private Response wrongDataResponse(String message) {
+		
+		return new Response(ResponseCode.WRONG_REQUEST_DATA, message);
+	}
+
+	private Response wrongTypeResponse(String requestType) {
+		
+		return new Response(ResponseCode.WRONG_REQUEST_TYPE, requestType);
+	}
+	
+
+	private Response getManagersWithMostFactor(String requestData) {
+		Manager[] managers = company.getManagersWithMostFactor();
+		return new Response(ResponseCode.OK, managersToJSON(managers));
+	}
+
+	private String managersToJSON(Manager[] managers) {
+		
+		return Arrays.stream(managers)
+				.map(Employee::getJSON)
+				.collect(Collectors.joining(";"));
+	}
+
+	private Response getDepartments(String requestData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Response getDepartmentBudget(String requestData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Response removeEmployee(String requestData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Response getEmployee(String requestData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Response addEmployee(String emplJSON) {
+		Employee empl = (Employee) new Employee().setObject(emplJSON);
+		company.addEmployee(empl);
+		return new Response(ResponseCode.OK, "");
+	}
+
+}
---
+++ src/main/java/telran/employees/net/CompanyServerAppl.java
@@ -0,0 +1,26 @@
+package telran.employees.net;
+
+import telran.employees.*;
+import telran.io.Persistable;
+import telran.net.Protocol;
+import telran.net.TcpServer;
+
+public class CompanyServerAppl {
+
+	private static final String FILE_NAME = "employeesTest.data";
+	private static final int PORT = 5000;
+
+	public static void main(String[] args) {
+		Company company = new CompanyMapsImpl();
+		try {
+			((Persistable)company).restore(FILE_NAME);
+		} catch (Exception e) {
+			
+		}
+		Protocol protocol = new CompanyProtocol(company);
+		TcpServer tcpServer = new TcpServer(protocol, PORT);
+		tcpServer.run();
+
+	}
+
+}
