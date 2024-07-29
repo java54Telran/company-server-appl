ac7bb186d1d36660c41f375549ef2669874eb5ac
Yuragranovsky <yuragranovsky@gmail.com>
2024-07-29 21:49:07 +0300

    CW #41 //TODO's for possible shutdown

--- src/main/java/telran/employees/net/CompanyServerAppl.java
+++ src/main/java/telran/employees/net/CompanyServerAppl.java
@@ -20,7 +20,13 @@ public class CompanyServerAppl {
 		}
 		Protocol protocol = new CompanyProtocol(company);
 		TcpServer tcpServer = new TcpServer(protocol, PORT);
+		//FIXME need to start TCPServer as a thread
 		tcpServer.run();
+		//TODO
+		//cycle with asking a user to enter shutdown for exit from the server
+		//regular while cycle with no using cli-view
+		//by entering "shutdown" you should call method shutdown of the TcpServer
+		//after shutdown you should perform saving the data into the file
 
 	}
 
